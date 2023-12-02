3fedccc0268113d43bfdec12c4922166da357b9e
leowang871 <152684376+leowang871@users.noreply.github.com>
2023-12-02 15:59:27 +0800

    Update text1.py

--- text1.py
+++ text1.py
@@ -1,29 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
-import time
+import time 
 
-stock = ["1101","2330"]
+stock=["1101","5347"]
 
 for i in range(len(stock)):
-  stockid = stock[i]
-  url = 'https://tw.stock.yahoo.com/quote/'+ stockid +'.TW'
+  stockid=stock[i]
+  url = "https://tw.stock.yahoo.com/quote/"+ stockid +".TW"
   r = requests.get(url)
-  soup = BeautifulSoup(r.text, 'html.parser')
-  price = soup.find('span', class_=['Fz(32px) Fw(b) Lh(1) Mend(16px) D(f) Ai(c) C($c-trend-up)','Fz(32px) Fw(b) Lh(1) Mend(16px) D(f) Ai(c)','Fz(32px) Fw(b) Lh(1) Mend(16px) D(f) Ai(c) C($c-trend-down)']).getText()
-  print(price)
-
-  if price == none:
-    url = 'https://tw.stock.yahoo.com/quote/'+ stockid +'.TWO'
+  soup = BeautifulSoup(r.text,'html.parser')
+  price = soup.find('span',class_=['Fz(32px) Fw(b) Lh(1) Mend(16px) D(f) Ai(c) C($c-trend-up)','Fz(32px) Fw(b) Lh(1) Mend(16px) D(f) Ai(c)','Fz(32px) Fw(b) Lh(1) Mend(16px) D(f) Ai(c) C($c-trend-down)'])
+  if price == None:
+    url = "https://tw.stock.yahoo.com/quote/"+ stockid +".TWO"
     r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    price = soup.find('span', class_=['Fz(32px) Fw(b) Lh(1) Mend(16px) D(f) Ai(c) C($c-trend-up)','Fz(32px) Fw(b) Lh(1) Mend(16px) D(f) Ai(c)','Fz(32px) Fw(b) Lh(1) Mend(16px) D(f) Ai(c) C($c-trend-down)']).getText()
-    price = price.getText()
-message = "股票"+stockid+" 即時股價為: "+ price
-token = '6420496655:AAFNmGxfTU_r8GjLrEp8xygQUTBb39YaBNk'
-chat_id="輸入你的 telegram id"
-url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}"
-requests.get(url)
-time.sleep(3)
+    soup = BeautifulSoup(r.text,'html.parser')
+    price = soup.find('span',class_=['Fz(32px) Fw(b) Lh(1) Mend(16px) D(f) Ai(c) C($c-trend-up)','Fz(32px) Fw(b) Lh(1) Mend(16px) D(f) Ai(c)','Fz(32px) Fw(b) Lh(1) Mend(16px) D(f) Ai(c) C($c-trend-down)'])
+
+  meg= "股票" + stockid + "的即時價格為:" + price.getText()
+  token = '6420496655:AAFNmGxfTU_r8GjLrEp8xygQUTBb39YaBNk'
+    # 使用者 id
+  chat_id="1722040126"
+  # bot 送訊息
+  url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={meg}"
+  requests.get(url)
+  time.sleep(3)
 
 
 
